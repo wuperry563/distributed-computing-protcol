1eacd702e253d0e319812bf2d701958aa5e7f160
Perry Wu <psw101020@dc01.utdallas.edu>
2020-07-06 13:40:11 -0500

    lmao

--- launcher.sh
+++ launcher.sh
@@ -7,6 +7,8 @@ netid=psw101020
 #
 # Root directory of your project
 PROJDIR=~/dev/CS6378/Project1
+echo $PROJDIR
+echo $netid
 #
 # This assumes your config file is named "config.txt"
 # and is located in your project directory
@@ -22,6 +24,8 @@ PROJDIR=~/dev/CS6378/Project1
 # Your main project class
 #
 PROG=Project1
+echo $PROG
+echo $1
 
 n=0
 
@@ -31,16 +35,20 @@ cat $1 | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
     echo $i
     
     k=$( echo $i | awk '{ print $1 }' )    
+	echo $k
     
     echo "k=" $k
     while read line 
     do
+	echo DO
         if [ $n -lt $k ]
 	then
 	host=$( echo $line | awk '{ print $2 }' )
+	echo host=$host
 	
 	nodeId=$( echo $line | awk '{ print $3 }' )
 	echo $netid "Running on" $host "with" $nodeId $n
+
         ssh -oStrictHostKeyChecking=no -l "$netid" "$host" "cd ; cd $PROJDIR; java Start $n $1 " &
 	fi
         n=$(( n + 1 ))
