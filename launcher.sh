7bb56b17e5e6d7492ae69e035f2846f02be8fb5c
perry wu <wuperry563@gmail.com>
2020-07-06 15:51:25 -0500

    lmao

--- launcher.sh
+++ launcher.sh
@@ -1,60 +1,34 @@
 #!/bin/bash
 
-
 # Change this to your netid
-netid=psw101020
+netid=sxg122830
 
-#
 # Root directory of your project
-PROJDIR=~/dev/CS6378/Project1
-echo $PROJDIR
-echo $netid
-#
-# This assumes your config file is named "config.txt"
-# and is located in your project directory
-#
-#CONFIG=$PROJDIR/NOT-THIS-YEARS-CONFIG.txt
-#CONFIG=config.txt (put it as command line argument)
-#
-# Directory your java classes are in
-#
-#BINDIR=$PROJDIR/bin
+PROJDIR=/people/cs/s/sxg122830/TestProj
 
-#
+# Directory where the config file is located on your local system
+CONFIGLOCAL=$HOME/launch/config.txt
+
+# Directory your java classes are in
+BINDIR=$PROJDIR/bin
+
 # Your main project class
-#
-PROG=Project1
-echo $PROG
-echo $1
+PROG=HelloWorld
 
 n=0
 
-cat $1 | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
+cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
 (
     read i
     echo $i
-    
-    k=$( echo $i | awk '{ print $1 }' )    
-	echo $k
-    
-    echo "k=" $k
-    while read line 
+    while [[ $n -lt $i ]]
     do
-	echo DO
-        if [ $n -lt $k ]
-	then
-	host=$( echo $line | awk '{ print $2 }' )
-	echo host=$host
-	
-	nodeId=$( echo $line | awk '{ print $3 }' )
-	echo $netid "Running on" $host "with" $nodeId $n
+    	read line
+    	p=$( echo $line | awk '{ print $1 }' )
+        host=$( echo $line | awk '{ print $2 }' )
 
-        ssh -oStrictHostKeyChecking=no -l "$netid" "$host" "cd ; cd $PROJDIR; java Start $n $1 " &
-	fi
+	gnome-terminal -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host java -cp $BINDIR $PROG $p; exec bash" &
+
         n=$(( n + 1 ))
     done
-   
 )
-
-
-
