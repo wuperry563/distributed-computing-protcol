3921f130c217e4166184eb91967916f049237417
perry wu <wuperry563@gmail.com>
2020-07-07 20:18:50 -0500

    whyyy

--- launcher.sh
+++ launcher.sh
@@ -4,16 +4,17 @@
 netid=psw101020
 
 # Root directory of your project
-PROJDIR=/home/013/p/ps/psw101020/dev/CS6378/Project1
+PROJDIR=~/dev/CS6378/Project1
 
 # Directory where the config file is located on your local system
-CONFIGLOCAL=$HOME/launch/config.txt
+CONFIGLOCAL=$HOME/CS6378/Project1/config.txt
 
 # Directory your java classes are in
-BINDIR=$PROJDIR/bin
+BINDIR=$PROJDIR
 
 # Your main project class
 PROG=Start
+
 n=0
 
 cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
@@ -25,7 +26,7 @@ cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
     	read line
     	p=$( echo $line | awk '{ print $1 }' )
         host=$( echo $line | awk '{ print $2 }' )
-
+	
 	gnome-terminal -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host java -cp $BINDIR $PROG $p; exec bash" &
 
         n=$(( n + 1 ))
